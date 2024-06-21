bc27aa6cbb7a5f363c35e898620652e108e610da
Paulo <paulorfsantos17@gmail.com>
2024-06-21 12:49:36 -0300

    feat: add create Atendimento

--- petland-domain/src/Main.java
+++ petland-domain/src/Main.java
@@ -1,6 +1,15 @@
+import java.time.LocalTime;
+import java.util.Date;
+
+import com.petland.domain.atendimento.Atendimento;
+import com.petland.domain.atendimento.AtendimentoStatus;
+import com.petland.domain.atendimento.AtendimentoTipo;
+import com.petland.domain.cadastro.Animal;
 import com.petland.domain.cadastro.Cadastro;
 import com.petland.domain.cadastro.Endereco;
+import com.petland.domain.cadastro.Especie;
 import com.petland.domain.cadastro.Perfil;
+import com.petland.domain.cadastro.ProdutoServico;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -20,10 +29,34 @@ public class Main {
 
         cadastro.setEndereco(endereco);
 
-        System.out.println("Cadastro criado, Nome: " + cadastro.getNome());
-        System.out.println("Endereco: " + cadastro.getEndereco().getLougadouro() + ", "
-                + cadastro.getEndereco().getNumero());
+        Animal animal = new Animal();
 
-        
+        animal.setId(1);
+        animal.setNome("Rex");
+        animal.setAniversario(new Date());
+        animal.setEspecie(Especie.CACHORRO);
+
+        ProdutoServico servico = new ProdutoServico();
+
+        servico.setId(1);
+        servico.setNome("Tosa");
+        servico.setValor(100.0);
+        servico.setServico(true);
+
+        Atendimento atendimento = new Atendimento();
+        atendimento.setId(1);
+        atendimento.setData(new Date());
+        atendimento.setHora(LocalTime.now());
+        atendimento.setEmergencia(false);
+        atendimento.setDescription(servico.getNome());
+        atendimento.setValor(servico.getValor());
+        atendimento.setStatus(AtendimentoStatus.REALIZANDO);
+        atendimento.setTipo(AtendimentoTipo.HIGIENIZACAO);
+        atendimento.setServico(servico);
+        atendimento.setPaciente(animal);
+        atendimento.setSolictante(cadastro);
+
+        System.out.println(atendimento);
+
     }
 }
