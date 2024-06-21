f4c6c5e62f8a4ba855643cdbbf829d75e4bd0720
Paulo <paulorfsantos17@gmail.com>
2024-06-21 09:46:48 -0300

    feat: add Edereco

---
+++ petland-domain/src/com/petland/domain/cadastro/Endereco.java
@@ -0,0 +1,22 @@
+package com.petland.domain.cadastro;
+
+public class Endereco {
+  private String lougadouro;
+  private String numero;
+
+  public String getLougadouro() {
+    return lougadouro;
+  }
+
+  public void setLougadouro(String lougadouro) {
+    this.lougadouro = lougadouro;
+  }
+
+  public String getNumero() {
+    return numero;
+  }
+
+  public void setNumero(String numero) {
+    this.numero = numero;
+  }
+}
